d2212363ddb6154a84ea5feb64afc36b2e505ba5
hesh <shihanhe@stu.pku.edu.cn>
2026-04-24 07:49:55 +0000

    save my priority draft

--- kernel/include/proc.h
+++ kernel/include/proc.h
@@ -52,7 +52,7 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
-
+  
   // these are private to the process, so p->lock need not be held.
   uint64 kstack;               // Virtual address of kernel stack
   uint64 sz;                   // Size of process memory (bytes)
@@ -64,6 +64,7 @@ struct proc {
   struct dirent *cwd;          // Current directory
   char name[16];               // Process name (debugging)
   int tmask;                    // trace mask
+  int static_prio;             // 增加
 };
 
 void            reg_info(void);
--- kernel/syscall.c
+++ kernel/syscall.c
@@ -120,6 +120,7 @@ extern uint64 sys_sysinfo(void);
 extern uint64 sys_rename(void);
 extern uint64 sys_shutdown(void);
 extern uint64 sys_dup2(void);
+extern uint64 sys_set_priority(void);
 
 static uint64 (*syscalls[])(void) = {
   [SYS_fork]        sys_fork,
@@ -150,6 +151,7 @@ static uint64 (*syscalls[])(void) = {
   [SYS_rename]      sys_rename,
   [SYS_shutdown]    sys_shutdown,
   [SYS_dup2]        sys_dup2,
+  [SYS_set_priority] sys_set_priority,
 };
 
 static char *sysnames[] = {
@@ -181,6 +183,7 @@ static char *sysnames[] = {
   [SYS_rename]      "rename",
   [SYS_shutdown]    "shutdown",
   [SYS_dup2]        "dup2",
+  [SYS_set_priority] "set_priority",
 };
 
 void
